5a3d668a92da9c8974dbd533a50357a9ccb01581
ravi.kumar <ravi.kumar@mail.rakuten.com>
2015-01-13 15:47:48 +0900

    added shortcuts for url

---
+++ app/models/url.rb
@@ -0,0 +1,4 @@
+class Url < ActiveRecord::Base
+  attr_accessible :url
+  validates :url, presence:  true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = 'c79a3a075a8426b1aa6f962ec90379a490690e7014a516ba51e72e3de6d98e4e4bc2d6530935d29d0052141584efd537213965bed3588d9c30c313aa0abe6f5a'
